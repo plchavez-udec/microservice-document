78124b4e9365375591b072b955b6d5c9af5d74f3
Pedro Luis <plchavez@ucundinamarca.edu.co>
2021-07-18 10:31:32 -0500

    Manejo de Timeout por medio de Hystrix

--- src/main/java/co/edu/ierdminayticha/sgd/documents/DocumentsApplication.java
+++ src/main/java/co/edu/ierdminayticha/sgd/documents/DocumentsApplication.java
@@ -3,6 +3,7 @@ package co.edu.ierdminayticha.sgd.documents;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 @SpringBootApplication
 public class DocumentsApplication {
